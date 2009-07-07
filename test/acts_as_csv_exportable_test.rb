800420b9f79119b1abdb39acf34bbd4e6f846ef1
Peter Leonhardt <peterleonhardt@gmail.com>
2009-07-07 10:37:09 -0500

    Fixing some formatting with the README and tests.

--- test/acts_as_csv_exportable_test.rb
+++ test/acts_as_csv_exportable_test.rb
@@ -12,32 +12,31 @@ class ActsAsCSVExportableTest < Test::Unit::TestCase
     @project.client = @client
   end
   
-    def test_project_default_template_is_set_as_defined_in_model
-      assert_equal Project.get_export_template(:default), [{:project_name => :name}, {:project_description => :description}, {:client_name => 'client.full_name'}, {:deadline => 'formatted_deadline'}]
-    end
-  
-    def test_project_simple_template_is_set_as_defined_in_model
-      assert_equal Project.get_export_template(:simple), [{:project_name => :name}]
-    end
-  
-    def test_to_csv_should_render_correct_results
-      assert_equal "Project Name,Project Description,Client Name,Deadline\nMy Project,Described,Joe Schmoe,#{@time.strftime('%m/%d/%Y')}\n", @client.projects.to_csv    
-    end
-    
-    def test_to_csv_using_proc_should_render_correct
-      assert_equal "Project Name,Deadline\nMy Project,#{@time.strftime('%m/%d/%Y')}\n", @client.projects.to_csv(:template => :proc)
-    end
-  
-    def test_to_csv_with_template_specified_should_render_correct
-      assert_equal "Project Name\nMy Project\n", @client.projects.to_csv(:template => :simple)
-    end
-  
-    def test_to_csv_works_when_columns_specified
-      assert_equal "Project Description,Project Name\n#{@project.description},#{@project.name}\n", @client.projects.to_csv(:columns => [{:project_description => :description}, {:project_name => :name}])
-    end
-  
-    def test_to_csv_works_with_chained_methods_when_columns_specified                                                                                                                                  
-      assert_equal "Project Description,Project Name,Client\n#{@project.description},#{@project.name},#{@project.client.full_name}\n",  @client.projects.to_csv(:columns => [{:project_description => :description}, {:project_name => :name}, {:client => "client.full_name"}])
-    end
+  def test_project_default_template_is_set_as_defined_in_model
+    assert_equal Project.get_export_template(:default), [{:project_name => :name}, {:project_description => :description}, {:client_name => 'client.full_name'}, {:deadline => 'formatted_deadline'}]
+  end
 
+  def test_project_simple_template_is_set_as_defined_in_model
+    assert_equal Project.get_export_template(:simple), [{:project_name => :name}]
+  end
+
+  def test_to_csv_should_render_correct_results
+    assert_equal "Project Name,Project Description,Client Name,Deadline\nMy Project,Described,Joe Schmoe,#{@time.strftime('%m/%d/%Y')}\n", @client.projects.to_csv    
+  end
+  
+  def test_to_csv_using_proc_should_render_correct
+    assert_equal "Project Name,Deadline\nMy Project,#{@time.strftime('%m/%d/%Y')}\n", @client.projects.to_csv(:template => :proc)
+  end
+
+  def test_to_csv_with_template_specified_should_render_correct
+    assert_equal "Project Name\nMy Project\n", @client.projects.to_csv(:template => :simple)
+  end
+
+  def test_to_csv_works_when_columns_specified
+    assert_equal "Project Description,Project Name\n#{@project.description},#{@project.name}\n", @client.projects.to_csv(:columns => [{:project_description => :description}, {:project_name => :name}])
+  end
+
+  def test_to_csv_works_with_chained_methods_when_columns_specified                                                                                                                                  
+    assert_equal "Project Description,Project Name,Client\n#{@project.description},#{@project.name},#{@project.client.full_name}\n",  @client.projects.to_csv(:columns => [{:project_description => :description}, {:project_name => :name}, {:client => "client.full_name"}])
+  end
 end
